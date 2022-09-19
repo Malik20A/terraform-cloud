702d1f65a4ec566cfc5b02c70e7f97568ea8db94
Malik20A <106113996+Malik20A@users.noreply.github.com>
2022-09-18 20:00:46 -0500

    fix11

--- rds/main.tf
+++ rds/main.tf
@@ -57,7 +57,6 @@ output "subnet1" {
 
 resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
-  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
   database_name           = "mydb"
   master_username         = "foo"
   master_password         = "bar"
