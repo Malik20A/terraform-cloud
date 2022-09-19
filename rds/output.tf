63bc7b5d954773381d3e3a9c7baa4920bc1e274d
Malik20A <106113996+Malik20A@users.noreply.github.com>
2022-09-19 16:26:34 -0500

    fix6

--- rds/output.tf
+++ rds/output.tf
@@ -2,16 +2,16 @@ output "writer_aws_rds_endpoint" {
   value = "Writer ${aws_rds_cluster.example.endpoint}"
 }
 
-output "reader_aws_rds_endpoint1" {
+output "reader1_aws_rds_endpoint1" {
   value = "Reader ${aws_rds_cluster.example.reader_endpoint}"
 }
 
 
-output "reader_aws_rds_endpoint" {
+output "reader2_aws_rds_endpoint" {
   value = "reader2 ${aws_rds_cluster_endpoint.reader2}"
 }
 
-output "reader_aws_rds_endpoint" {
+output "reader3_aws_rds_endpoint" {
   value = "reader3 ${aws_rds_cluster_endpoint.reader3}"
 }
 
