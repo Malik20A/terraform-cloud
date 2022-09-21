498065d5e58fc3e793a37d9938463006a3e9900e
Malik20A <quepasadewie@gmail.com>
2022-09-20 23:04:45 -0500

    fix

---
+++ VPC/VPC/module/variable.tf
@@ -0,0 +1,18 @@
+variable "region" {}
+variable "cidr_block" {}
+variable "s3_bucket" {}
+variable "s3_folder_type" {}
+variable "s3_folder_region" {}
+variable "s3_tfstate_file" {}
+variable "s3_folder_project" {}
+variable "cidr_block1_public" {}
+variable "cidr_block2_public" {}
+variable "cidr_block3_public" {}
+
+variable "cidr_block1_private" {}
+variable "cidr_block2_private" {}
+variable "cidr_block3_private" {}
+
+variable "az1" {}
+variable "az2" {}
+variable "az3" {}
